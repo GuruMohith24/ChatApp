50b35c0c89ac2d1df72428d3b92a5992004fe6e6
GuruMohith24 <rangannagari.guru2023@vitstudent.ac.in>
2026-06-04 20:58:10 +0530

    feat: add chat history query to MessageRepository

--- chatapp/src/main/java/chatapp/com/chatapp/repository/MessageRepository.java
+++ chatapp/src/main/java/chatapp/com/chatapp/repository/MessageRepository.java
@@ -1,10 +1,16 @@
 package chatapp.com.chatapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import chatapp.com.chatapp.model.Message;
+import chatapp.com.chatapp.model.User;
 
 public interface MessageRepository extends JpaRepository<Message , Long >{
 
-    
+    List<Message> findBySenderAndRecipientOrRecipientAndSenderOrderByCreatedAtAsc(
+        User sender1, User recipient1, User sender2, User recipient2
+    );
+
 } 
