3da4cc1ab7a14da9b003dd27752911488130afc1
GuruMohith24 <rangannagari.guru2023@vitstudent.ac.in>
2026-06-14 21:29:19 +0530

    feat:created frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1>ChatApp</h1>
+      <label>Username</label>
+      <input type="text" placeholder="Enter username" />
+      <label>Password</label>
+      <input type="password" placeholder="Enter password" />
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default App
